fad858cb76dc8ba6f3a19f6618a88815df2135ef
Sebastian Erazo Ochoa <129110237+Sebas41@users.noreply.github.com>
2025-10-27 12:49:21 -0500

    feat(users): Implemented users module

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+import { UserRole } from '../../common/enums/role.enum';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'student@example.com' })
+  @IsEmail()
+  email!: string;
+
+  @ApiProperty({ minLength: 6 })
+  @IsString()
+  @MinLength(6)
+  password!: string;
+
+  @ApiProperty({ example: 'Jane Doe' })
+  @IsString()
+  fullName!: string;
+
+  @ApiProperty({ enum: UserRole, required: false })
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+}
